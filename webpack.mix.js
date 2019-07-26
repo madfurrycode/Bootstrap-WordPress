e4257a1c91c37981e5220dc5baa0dabaa15ac91d
Tristan <tristandevelliott1996@gmail.com>
2019-07-25 17:04:45 -0700

    Initial setup

---
+++ webpack.mix.js
@@ -0,0 +1,35 @@
+/*
+ * Bootstrap WP uses Laravel Mix
+ *
+ * Check the documentation at
+ * https://laravel.com/docs/5.6/mix
+ */
+
+let mix = require( 'laravel-mix' );
+
+mix.browserSync({
+	proxy: 'http://bootstrapwp.co.za/',
+	files: [
+		'**/*.php',
+		'assets/dist/css/**/*.css',
+		'assets/dist/js/**/*.js',
+    '**/*.css'
+	],
+	injectChanges: true,
+	open: false
+});
+
+// Autloading jQuery.
+mix.autoload({
+	jquery: ['$', 'window.jQuery', 'jQuery'],
+});
+
+mix.setPublicPath( './assets/dist' );
+
+// Compile assets
+mix.sass( 'assets/src/sass/style.scss', 'style.css' );
+
+// Add versioning to assets in production environment
+if ( mix.inProduction() ) {
+	mix.version();
+}
